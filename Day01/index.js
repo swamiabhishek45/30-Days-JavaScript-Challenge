b5558684de7aa439274fca8f3b5738a05223d6a7
swamiabhishek45 <abhishekswami1435@gmail.com>
2024-07-13 08:43:17 +0530

    Day01 Task 5 done

---
+++ Day01/index.js
@@ -0,0 +1,45 @@
+// Activity 1 : Variable Declaration
+
+var num = 7789;
+console.log(num);
+
+let str = "Abhishek"
+console.log(str);
+
+// Activity 2 : Constant Declaration
+
+const booleanVal = true;
+console.log(booleanVal);
+
+// Activity 3 : Data Types
+
+const digit = 234;
+const name = "Rahul";
+const isVerified = false;
+const person= {
+    name: "Satish",
+    age: 34
+}
+const fruits = ["Apple", "Banana", "Orange", "Guava"];
+
+console.log(typeof digit);
+console.log(typeof name);
+console.log(typeof isVerified);
+console.log(typeof person);
+console.log(typeof fruits);
+
+// Activity 4 : Reassigning Variables
+
+let val = 'Nitin';
+console.log(val);
+
+val = 'Putin';
+console.log(val);
+
+// Activity 5 : Understanding const
+
+const pi = 3.14;
+console.log(pi);
+
+pi = 3.15; // TypeError: Assignment to constant variable.
+console.log(pi);
